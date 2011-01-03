a99b75908ae78d52af6497758c6ecdb0d05f4d18
Joe Redmon <pjreddie@gmail.com>
2011-01-03 16:39:07 -0500

    Perspectivegit merge origin/master

--- Golden Triangle/MyOpenGLView.m
+++ Golden Triangle/MyOpenGLView.m
@@ -7,28 +7,49 @@
 //
 
 #import "MyOpenGLView.h"
-#include <OpenGL/gl.h>
-
+#import <OpenGL/OpenGL.h>
+#import <OpenGL/gl.h>
+#import <OpenGL/glu.h>
 
 @implementation MyOpenGLView
 
+static void drawTriangle(float r, float g, float b){
+	glBegin(GL_POLYGON);
+	glColor3f( r, g, b );
+	glVertex3f(0.0, 0.0, -4.0);
+	glVertex3f(0.0, 1.0, -4.0);
+	glVertex3f(1.5, 1.0, -4.0);
+    glEnd();
+}
+
 static void drawAnObject () 
 {
-	glColor3f(1.0f, 0.85f, 0.35f);
-	glBegin(GL_QUADS);
-	{
-		glVertex4f(-1, -1, 0, 0);
-		glVertex4f(-1, 1, 0, 0);
-		glVertex4f(1, 1, 0, 0);
-		glVertex4f(1, -1, 0, 0);
-		
-	}
-	glEnd();
+	drawTriangle(1, 0, 0);
+}
+
+/*
+ * Resize ourself
+ */
+- (void) reshape
+{ 
+	NSRect sceneBounds;
+	
+	[ [ self openGLContext ] update ];
+	sceneBounds = [ self bounds ];
+	// Reset current viewport
+	glViewport( 0, 0, sceneBounds.size.width, sceneBounds.size.height );
+	glMatrixMode( GL_PROJECTION );   // Select the projection matrix
+	glLoadIdentity();                // and reset it
+	// Calculate the aspect ratio of the view
+	gluPerspective( 45.0f, sceneBounds.size.width / sceneBounds.size.height,
+                   0.1f, 100.0f );
+	glMatrixMode( GL_MODELVIEW );    // Select the modelview matrix
+	glLoadIdentity();                // and reset it
 }
 
 
-
 -(void) drawRect:(NSRect) bounds {
+	[self reshape];
 	glClearColor(0,0,0,0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	drawAnObject();
