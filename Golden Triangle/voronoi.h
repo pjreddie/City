0cf1af4f0bfff8cbcbf2e593ba3f795c002c2d0c
Joe Redmon <pjreddie@gmail.com>
2011-01-09 20:33:15 -0500

    Voronoi Diagrams Working!!!

---
+++ Golden Triangle/voronoi.h
@@ -0,0 +1,25 @@
+/*
+ *  voronoi.h
+ *  Golden Triangle
+ *
+ *  Created by Joe Redmon on 1/4/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+struct Point;
+
+struct Line;
+
+struct Segment;
+
+struct DPoint;
+
+struct DEdge;
+
+struct DFace;
+
+struct DCEL;
+
+void drawV();
