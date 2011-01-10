230a84d71bc9d5b737fea252e765dc4203300cc9
abullrd <abullrd@gmail.com>
2011-01-09 23:56:32 -0500

    min and max in generatevoronoi were mixed up something is still wrong in getpolygons at s.pop_front();

--- Golden Triangle/voronoi.cpp
+++ Golden Triangle/voronoi.cpp
@@ -11,18 +11,17 @@
 
 
 double randf(double high, double low){
-	int r = rand();
+	int r = arc4random();
 	r = r%10000;
 	double f = r/10000.0;
 	double dist = high-low;
 	return f*dist + low;
 }
 Voronoi GenerateVoronoi(int seed, int numControl, double minx, double maxx, double miny, double maxy){
-	
-	srand(seed);
+	//srand(seed); dont need w/ arc4random i think
 	Voronoi d;
 	for(int i = 0; i < numControl; ++i){
-		d.addPoint(JPoint(randf(minx,maxx),randf(miny,maxy)));
+		d.addPoint(JPoint(randf(maxx,minx),randf(maxy, miny)));
 	}
 	return d;
 	
